ebfc607821fae931b4e02b450c19e201738dd566
DmitriySidorov <dmitriysidorov@gmail.com>
2017-07-06 00:49:45 +0300

    ShopDress

---
+++ js/index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by SD on 27.05.2017.
+ */
+$(function() {
+
+	$('#example').barrating({
+		theme: 'fontawesome-stars'
+	});
+	$('#example2').barrating({
+		theme: 'fontawesome-stars'
+	});
+	$('#example3').barrating({
+		theme: 'fontawesome-stars'
+	});
+
+});
